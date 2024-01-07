cfc5765bb400867b099c0adec8cb850fc9eb1820
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2024-01-07 02:33:23 -0500

    clean up webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    // filename: 'main.js',
     filename: "bundle.js",
-
     path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(?:js|mjs|cjs)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-            ]
-          }
-        }
-      },
-    ],
   },
 };
