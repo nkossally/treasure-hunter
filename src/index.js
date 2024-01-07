e1ab2265fc935b4e4c12b213c5b6c2455045ece2
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2024-01-07 02:02:33 -0500

    babel

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
-import MiniTroll from './minitroll.png';
-import Slider from "./Slider.js";
-import solution from "./solution.js";
-import challenge from "./challenge.js";
 
 function component() {
   const element = document.createElement("div");
-  const s1 = document.createElement("script");
-  const s2 = document.createElement("script");
-  const s3 = document.createElement("script");
-
-  s1.src = Slider
-  s2.src = solution
-  s3.src = challenge
-
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-  const minitroll = new Image();
-  minitroll.src = MiniTroll;
-
-  element.appendChild(minitroll);
 
   return element;
 }
-
-document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,19 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
       },
     ],
   },
